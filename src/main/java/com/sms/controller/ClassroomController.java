6ae1385add1f3cf14644d31db37adff1bcc56844
Hansaja Sandeepa <76199476+hansajasandeepabadalge@users.noreply.github.com>
2025-03-17 16:29:03 +0530

    Updated Classroom Services

---
+++ src/main/java/com/sms/controller/ClassroomController.java
@@ -0,0 +1,72 @@
+package com.sms.controller;
+
+import com.sms.model.Classroom;
+import com.sms.service.ClassroomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ClassroomController {
+    
+    @Autowired
+    private ClassroomService classroomService;
+
+    // Get all the classroom in the database
+    @GetMapping("/classrooms")
+    public List<Classroom> getAllClassrooms() {
+        return classroomService.getAllClassrooms();
+    }
+
+    // Get a classroom details related to the ID
+    @GetMapping("/classroom/{id}")
+    public ResponseEntity<?> getClassroomById(@PathVariable Long id) {
+        Classroom classroom = classroomService.getClassroomById(id);
+        if (classroom != null) {
+            return ResponseEntity.ok(classroom);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Classroom not found " + id + " not found");
+        }
+    }
+
+    // Create a new classroom
+    @PostMapping("/classroom")
+    public Classroom createClassroom(@RequestBody Classroom classroom) {
+        return classroomService.addClassroom(classroom);
+    }
+
+    // Update existing classrooms' details
+    @PutMapping("/classroom/{id}")
+    public ResponseEntity<?> updateStudent(@PathVariable Long id, @RequestBody Classroom classroom) {
+        if (!id.equals(classroom.getClassId())) {
+            return ResponseEntity.badRequest().body("Path ID and request body ID do not match" + id + " " + classroom.getClassId());
+        }
+        // Getting Updated classroom details
+        Classroom updatedClassroom = classroomService.updateClassroom(classroom);
+
+        //  and give a response
+        if (updatedClassroom != null) {
+            return ResponseEntity.ok(updatedClassroom);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Classroom with ID " + id + " not found");
+        }
+    }
+
+    // Delete classroom by ID
+    @DeleteMapping("/classroom/{id}")
+    public ResponseEntity<?> deleteClassroomById(@PathVariable Long id) {
+        Classroom classroom = classroomService.getClassroomById(id);
+
+        // Check the ID related classroom is in the database or not and delete
+        if (classroom != null) {
+            classroomService.deleteClassroom(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Classroom with ID " + id + " deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Classroom with ID " + id + " not found");
+        }
+    }
+}
--- src/main/java/com/sms/repository/ClassroomRepository.java
+++ src/main/java/com/sms/repository/ClassroomRepository.java
@@ -2,6 +2,8 @@ package com.sms.repository;
 
 import com.sms.model.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
 }
